39c91f9f636a8c7c732927a8cbf8f4b85fe0d57c
Hungdinhz <phunghung12112005@gmail.com>
2025-10-24 07:51:30 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pdh_231230796_de01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
